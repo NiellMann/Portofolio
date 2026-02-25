39164f167b28002069ca64b7758397a3cbaa1f88
Daniel Imanuel Manafe <danielimanuelmanafe@gmail.com>
2026-02-25 17:53:16 +0700

    Fix image path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -252,7 +252,7 @@ function App() {
       <main>
         {/* Profile Header */}
         <div className="profile-header">
-          <img src="/assets_foto/logo/IMG-20241228-WA0022.jpg" alt="Profile" className="profile-avatar" />
+          <img src="/Portofolio/assets_foto/logo/IMG-20241228-WA0022.jpg" alt="Profile" className="profile-avatar" />
           <div className="profile-info">
             <h1>Daniel Imanuel Manafe</h1>
             <p className="username">Automation & Instrumentation Engineer</p>
